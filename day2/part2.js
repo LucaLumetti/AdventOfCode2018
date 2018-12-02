0c058f7d08019166c68fcfffb6571f24ee852f33
Luca Lumetti <lumetti.luca@gmail.com>
2018-12-02 14:50:17 +0100

    cleanup

--- day2/part2.js
+++ day2/part2.js
@@ -2,29 +2,20 @@ const fs = require('fs')
 let input = fs.readFileSync('input', 'utf-8')
 input = input.split('\n')
 input.pop()
-
 let strs = input.splice(0)
 
-let strs_cpy = [...strs]
-let checks = []
-let res = []
-
-strs.forEach((s)=>{
-  for(let i = 0; i < strs_cpy.length; i++){
-    let k = s.split('').map((c,j)=>{
-      return Math.abs(c.charCodeAt(0) - strs_cpy[i].split('')[j].charCodeAt(0))
-    }).join('')
-    if(k.replace(/[0]/g,'').length === 1)
-      res.push(s)
+for(let i = 0; i < strs.length; i++){
+  for(let j = i; j < strs.length; j++){
+    let diff = 0
+    let index = -1
+    for(let c in strs[i]){
+      diff += strs[i][c] !== strs[j][c]
+      index = c
+    }
+    if(diff === 1){
+      let result = strs[i].split('').filter((a,i)=>i!==index).join('')
+      console.log(result)
+      return
+    }
   }
-})
-
-let fstr = []
-res[1] = res[1].split('')
-res[0].split('').forEach((c,i)=>{
-  if(c === res[1][i]){
-    fstr.push(c)
-  }
-})
-
-console.log(fstr.join(''))
+}
