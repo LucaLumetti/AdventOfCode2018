48dc018c2dfa9a3bc9645abb380839b076c34e14
Luca Lumetti <lumetti.luca@gmail.com>
2018-12-05 14:39:30 +0100

    cool solution for day1part1

--- day1/part1.js
+++ day1/part1.js
@@ -1,5 +1,1 @@
-let fs = require('fs')
-let input = fs.readFileSync(__dirname + '/input', 'utf-8').split('\n').map(Number)
-
-let res = input.reduce((a,b)=>a+b)
-console.log(res)
+console.log(eval(require('fs').readFileSync('input', 'utf-8')))
